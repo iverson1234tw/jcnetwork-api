363f3234c257640bd74bc1282ce634565234774d
iverson1234tw <reno65072013@gmail.com>
2020-05-06 09:48:00 +0800

    add flask Response

--- app_run.py
+++ app_run.py
@@ -2,17 +2,17 @@
 # coding: utf8
 
 from werkzeug.exceptions import HTTPException
-from flask import request, send_file, json
+from flask import request, send_file, json, Response
 from __init__ import *
 
 @app.route('/try-jcnetwork-api', methods=['GET', 'POST', 'DELETE'])
 def try_jcnetwork_api():
-    request.mimetype = 'application/json'
     if request.method == 'GET':
-        return json.dumps({
+        jc_res = {
             'code': 200,
             'message': 'Success to call the get method!'
-            })
+        }
+        return Response(json.dumps(jc_res), mimetype='application/json')
     elif request.method == 'POST':
         print(json.loads(request.get_data()).get('text'))
         name = json.loads(request.get_data()).get('text')
